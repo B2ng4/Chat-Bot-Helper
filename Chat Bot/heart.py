c8c08f23b0648c39e093bab807b2e920ed92e90c
B2ng4 <125038202+B2ng4@users.noreply.github.com>
2023-12-12 10:34:23 +1000

    Create

--- Chat Bot/heart.py
+++ Chat Bot/heart.py
@@ -1,2 +1,18 @@
 import vk_api
 from vk_api.longpool import VkLongPoll, VkEventType
+
+
+vk_session = vk_api.VkApi(token = 'vk1.a.BHU7W_4aXKU-DjsygLxNJ6H2Mq9aVHGNlC-F6gmrhJeFmZF8hUsE-Y9_eaprFKJsSfJZJ9FC0doqFLW8_AytL-XqJY3W5huVsGpFHfXT3KEjN1V2xJoLaopwLXX_FcOwEiukY3il3V0oCEL2KSQXqI1NPhsgP3VEPayIhPYOEQLsXwaLxhwgisEBgHBCK9ikoFd4P93Suatj2zZWy5I3bg')
+session_api = vk_session.get_api()
+longpool = VkLongPoll(vk_session)
+
+def send_some_msg(id, some_text):
+    vk_session.method("messages.send", {"user_id":id, "message":some_text,"random_id":0})
+
+for event in longpool.listen():
+    if event.type == VkEventType.MESSAGE_NEW:
+        if event.to_me:
+            msg = event.text.lower()
+            id = event.user_id
+            if msg == "hi":
+                send_some_msg(id, "Hi friend!")
