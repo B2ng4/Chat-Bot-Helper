f1ce24c1f2db356ff081c6dd2d12d610a8430e3f
B2ng4 <125038202+B2ng4@users.noreply.github.com>
2023-12-13 14:27:40 +1000

    Update heart.py

--- Chat Bot/heart.py
+++ Chat Bot/heart.py
@@ -19,7 +19,7 @@ def send_message(user_id, message, keyboard=None):
 def create_keyboard():
     keyboard = VkKeyboard(one_time=True)
 
-    keyboard.add_button('Подача обращения', color=VkKeyboardColor.PRIMARY)
+
     keyboard.add_button('Ответы на вопросы', color=VkKeyboardColor.PRIMARY)
     keyboard.add_line()
     keyboard.add_button('Сообщить о проблеме', color=VkKeyboardColor.NEGATIVE)
@@ -45,63 +45,7 @@ for event in longpoll.listen():
             welcome_message = 'Здравствуйте! Я умный цифровой помощник главы города Мирный. Что Вас интересует?'
             send_message(user_id, welcome_message, keyboard)
 
-        elif message == 'подача обращения':
-            send_message(user_id, 'Вы выбрали "Подача обращения"')
-            send_message(user_id, 'Запишите ваше ФИО')
-            def name():
-                for event in longpoll.listen():
-                    if event.type == VkEventType.MESSAGE_NEW and event.to_me:
-                        user_id = event.user_id
-                        full_name = event.text.lower()
-                        send_message(user_id, f'Ваше введенное ФИО {full_name}')
-                        break
-            # name()
 
-            send_message(user_id, 'Запишите ваш населенный пункт')
-            def city():
-                for event in longpoll.listen():
-                    if event.type == VkEventType.MESSAGE_NEW and event.to_me:
-                        user_id = event.user_id
-                        input_city = event.text.lower()
-                        send_message(user_id, f'Ваш введенный населенный пункт {input_city}')
-                        break
-            # city()
-
-            send_message(user_id, 'Запишите вашу электронную почту для обратной связи')
-            def email():
-                for event in longpoll.listen():
-                    if event.type == VkEventType.MESSAGE_NEW and event.to_me:
-                        user_id = event.user_id
-                        input_mail = event.text.lower()
-                        send_message(user_id, f'Ваша введенная электронная почта {input_mail}')
-                        break
-            # email()
-
-            send_message(user_id, 'Прикрепите обращение')
-
-
-            def download_file(url, file_name, path_to_save, access):
-                try:
-                    headers = {'Authorization': access}
-                    response = requests.get(url, headers=headers)
-                    if response.status_code == 200:
-                        file_path = os.path.join(path_to_save, file_name)
-                        with open(file_path, 'wb') as file:
-                            file.write(response.content)
-                        print("File downloaded successfully!")
-                    else:
-                        print("Failed to download file. Status code:", response.status_code)
-                except Exception as e:
-                    print("An error occurred during file download:", str(e))
-
-
-            # Usage example
-            url = "https://api.vk.com/method/docs.getUploadServer"
-            access = 'access_token=vk1.a.FyHTGxC2QiBjahv1ysRJSRNk0tfc1vUmNezj_A0SXBrmodK7G1iPmc-q39FujXH4igSDJtNie7_jklwW-y2w8p_SNfSAbZZXLh5VoL2MeTiE_fSChGqLxUy1RGN19xJtPSMqeHrzHE2dEcc1Qk5MU_y4V7aMyZ-Bims-9yixntUjSWg0aoRebbi7zFBlSK68DWN6IPGCNqBoNkKgaomJfw'
-            file_name = "downloaded_file"
-            path_to_save = "/home/konstantin/Документы/GitHub/Chat-Bot-Helper/Chat Bot/doc"
-
-            download_file(url, file_name, path_to_save, access)
 
         elif message == 'ответы на вопросы':
             send_message(user_id, 'Вы выбрали "Ответы на вопросы"',keyboard)
